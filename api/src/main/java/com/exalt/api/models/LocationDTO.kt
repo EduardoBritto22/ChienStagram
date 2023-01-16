4295038d1139d965c1f64f322ba4f7fdc946855d
Eduardo Britto <eduardoobritto@gmail.com>
2023-01-16 17:39:18 +0100

    Add models and DTOs for detailed Post and User. Add DTO for Location.

---
+++ api/src/main/java/com/exalt/api/models/LocationDTO.kt
@@ -0,0 +1,8 @@
+package com.exalt.api.models
+
+data class LocationDTO(
+    val street: String,
+    val city: String,
+    val state: String,
+    val country: String
+)
---
+++ domain/src/main/java/com/exalt/domain/home/models/OwnerModel.kt
@@ -0,0 +1,12 @@
+package com.exalt.domain.home.models
+
+data class OwnerModel(
+    val id: String,
+    val name: String,
+    val email: String,
+    val phone: String,
+    val gender: String,
+    val Address: String,
+    val dateOfBirth: String,
+    val pictureUrl: String
+)
