803546f7b8d5f32b71fad4f5edb445d62ecf2a5b
Eduardo Britto <eduardoobritto@gmail.com>
2023-01-17 16:28:44 +0100

    Add Location Model and its mapper.

---
+++ data/src/main/java/com/exalt/data/mappers/LocationMapper.kt
@@ -0,0 +1,10 @@
+package com.exalt.data.mappers
+
+import com.exalt.api.models.LocationDTO
+import com.exalt.domain.home.models.LocationModel
+import javax.inject.Inject
+
+class LocationMapper @Inject constructor() {
+    fun fromDto(location: LocationDTO): LocationModel =
+        LocationModel("${location.street}\n${location.city}-${location.state}\n${location.country}")
+}
