d2d9ec442f5aeb530c9b9a6995770e85bd71dcbe
Eduardo Britto <eduardoobritto@gmail.com>
2023-01-17 11:09:52 +0100

    Add tests for the use cases.

--- domain/src/main/java/com/exalt/domain/home/models/DomainModelFactory.kt
+++ domain/src/main/java/com/exalt/domain/home/models/DomainModelFactory.kt
@@ -4,13 +4,27 @@ object DomainModelFactory {
     const val POST_ID = "POST_ID"
     const val POST_TEXT = "POST_TEXT"
     const val POST_IMAGE_URL = "POST_IMAGE_URL"
+    const val POST_LINK_URL = "POST_URL"
     const val POST_PUBLISH_DATE = "POST_PUBLISH_DATE"
+    const val POST_LIKES = 100
 
     const val OWNER_ID = "OWNER_ID"
     const val OWNER_TITLE = "OWNER_TITLE"
     const val OWNER_FIRST_NAME = "OWNER_FIRST_NAME"
     const val OWNER_LAST_NAME = "OWNER_LAST_NAME"
     const val OWNER_PICTURE_URL = "OWNER_PICTURE_URL"
+    const val OWNER_PHONE = "OWNER_PHONE"
+    const val OWNER_EMAIL = "OWNER_EMAIL"
+    const val OWNER_GENDER_MALE = "male"
+    const val OWNER_GENDER_FEMALE = "female"
+    const val OWNER_GENDER_OTHER = "other"
+    const val OWNER_BIRTHDATE = "20/08/1958"
+    const val OWNER_ADDRESS = "5225, Hansemyrveien\nVingrom Finnamasrk - FinnmAirku\nNorway"
+
+
+    const val COMMENT_ID = "COMMENT_ID"
+    const val COMMENT_MESSAGE = "COMMENT_MESSAGE"
+    const val COMMENT_PUBLISH_DATE = "20/08/2021"
 
     fun getDefaultPostPreviewModel(
         id: String = POST_ID
@@ -22,7 +36,53 @@ object DomainModelFactory {
         owner = getDefaultOwnerPreviewModel(),
     )
 
+    fun getDefaultPostModel(
+        id: String = POST_ID
+    ) = PostModel(
+        id = id,
+        text = POST_TEXT,
+        imageUrl = POST_IMAGE_URL,
+        publishDate = POST_PUBLISH_DATE,
+        likes = POST_LIKES,
+        link = POST_LINK_URL,
+        tags = emptyList(),
+        owner = getDefaultOwnerPreviewModel(),
+    )
+
     fun getDefaultOwnerPreviewModel() = OwnerPreviewModel(
-        id = OWNER_ID, name = "$OWNER_FIRST_NAME $OWNER_LAST_NAME", pictureUrl = OWNER_PICTURE_URL
+        id = OWNER_ID,
+        name = "$OWNER_FIRST_NAME $OWNER_LAST_NAME",
+        pictureUrl = OWNER_PICTURE_URL
+    )
+
+    fun getDefaultOwnerModel() = OwnerModel(
+        id = OWNER_ID,
+        name = "$OWNER_FIRST_NAME $OWNER_LAST_NAME",
+        pictureUrl = OWNER_PICTURE_URL,
+        Address = OWNER_ADDRESS,
+        dateOfBirth = OWNER_BIRTHDATE,
+        email = OWNER_EMAIL,
+        gender = OWNER_GENDER_MALE,
+        phone = OWNER_PHONE
+    )
+    fun getDefaultCommentModel(
+        id: String = COMMENT_ID
+    ) = CommentModel(
+        id = id,
+        owner = getDefaultOwnerPreviewModel(),
+        message = COMMENT_MESSAGE,
+        post = POST_ID,
+        publishDate = COMMENT_PUBLISH_DATE
+
+    )
+    fun getDefaultOwnerModelWithGender(gender: String) = OwnerModel(
+        id = OWNER_ID,
+        name = "$OWNER_FIRST_NAME $OWNER_LAST_NAME",
+        pictureUrl = OWNER_PICTURE_URL,
+        Address = OWNER_ADDRESS,
+        dateOfBirth = OWNER_BIRTHDATE,
+        email = OWNER_EMAIL,
+        gender = gender,
+        phone = OWNER_PHONE
     )
 }
